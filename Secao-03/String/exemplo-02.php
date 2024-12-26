ed1a20d90fdaf1594ae24b2cb65b195b67b0078f
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-26 18:05:42 -0300

    Exemplos de strings

---
+++ Secao-03/String/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = 'wesley dos anjos';
+echo $name .'<br>';
+echo strtoupper($name) .'<br>';
+echo mb_strtoupper($name) .'<br>';
+echo strtolower($name) .'<br>';
+echo mb_strtolower($name) .'<br>';
+echo ucfirst($name) .'<br>';
+echo ucwords($name) .'<br>';
+
+?>
---
+++ Secao-03/String/exemplo-03.php
@@ -0,0 +1,8 @@
+<?php
+
+$company = 'Hcode';
+$company = str_replace('o', '0', $company);
+$company = str_replace('e', '3', $company);
+echo $company .'<br>';
+
+?>
