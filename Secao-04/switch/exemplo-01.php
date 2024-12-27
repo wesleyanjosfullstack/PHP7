0bd526bbc7611b05c5ee5f98baee6e9d386e3b0f
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-27 18:36:33 -0300

    Atualizando switch case

--- Secao-04/switch/exemplo-01.php
+++ Secao-04/switch/exemplo-01.php
@@ -1,6 +1,16 @@
 <?php
 
 $dayWeek = date('w');
-echo $dayWeek;
+
+switch($dayWeek) {
+    case 0: echo 'Domingo !<br>'; break;
+    case 1: echo 'Segunda-feira !<br>'; break;
+    case 2: echo 'Terça-feira !<br>'; break;
+    case 3: echo 'Quarta-feira !<br>'; break;
+    case 4: echo 'Quinta-feira !<br>'; break;
+    case 5: echo 'Sexta-feira !<br>'; break;
+    case 6: echo 'Sabado !<br>'; break;
+    default: echo 'Não encontrado'; break;
+}
 
 ?>
