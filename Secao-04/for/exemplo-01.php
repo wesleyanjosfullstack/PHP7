1bf49cd27e44d6ef520827c9aa8584db6f19d92a
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-27 18:51:23 -0300

    Exemplo de comando for
    Comando for r for com if mais exemplo de loop infiinito

--- Secao-04/for/exemplo-01.php
+++ Secao-04/for/exemplo-01.php
@@ -2,4 +2,19 @@
 
 for ($c = 0; $c < 11; $c++) echo $c .' '; 
 
+echo '<hr>';
+
+for ($c = 0; $c <= 1000; $c += 5) echo $c .' '; 
+echo '<hr>';
+
+for ($c = 0; $c <= 1000; $c += 5) {
+    if ($c > 200 && $c < 800) continue;
+    if ($c === 900) break;
+    echo $c .' ';
+} 
+
+echo '<hr>';
+
+for ($c = 100; $c > 0; $c -= 5) echo $c .' ';
+
 ?>
---
+++ Secao-04/for/exemplo-02.php
@@ -0,0 +1,6 @@
+<?php
+
+// Loop infinito
+// for ($c = 0; $c < 10; $c--) {
+//     echo $c .' ';
+// }
