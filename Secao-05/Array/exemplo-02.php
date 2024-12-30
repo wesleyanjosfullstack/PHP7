d0365e785cb15c67526b71154068dd6af6ea0ecd
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-30 12:17:31 -0300

    Array bidimensional

---
+++ Secao-05/Array/exemplo-02.php
@@ -0,0 +1,17 @@
+<?php
+
+$cars[0][0] = 'GM';
+$cars[0][1] = 'Cobalt';
+$cars[0][2] = 'Ônix';
+$cars[0][3] = 'Camaro';
+
+$cars[1][0] = 'Ford';
+$cars[1][1] = 'Fiesta';
+$cars[1][2] = 'Fusion';
+$cars[1][3] = 'Eco Sport';
+
+// print_r($cars);
+echo $cars[0][3] .'<br>';
+echo end($cars[1]) .'<br>';
+
+?>
