76cda518366446babc208c0e82ecb2e6048896a1
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-02 16:54:08 -0300

    Verificando o status da sessao e respondendo

--- Secao-06/session/exemplo-05.php
+++ Secao-06/session/exemplo-05.php
@@ -2,8 +2,23 @@
 
 require_once './config.php';
 
-echo session_save_path();
+echo session_save_path() .'<br>';
 
 var_dump(session_status());
+echo '<br>';
 
+switch(session_status()) {
+    case PHP_SESSION_DISABLED:
+        echo 'As sessões estiverem desabilitadas.<br>';
+        break;
+    case PHP_SESSION_NONE:
+        echo 'As sessões estiverem habilitadas, mas nenhuma existir..<br>';
+        break;
+    case PHP_SESSION_ACTIVE:
+        echo 'As sessões estiverem habilitadas, e uma existir.<br>';
+        break;
+    default:
+        echo 'Não encontrado.<br>';
+        break;
+}
 ?>
